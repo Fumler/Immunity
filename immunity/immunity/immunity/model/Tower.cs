f2fd72eb9b6411e1196dac787fc92a1250fe3713
Fredrik Pettersen <fredrik.fumler@gmail.com>
2012-10-20 04:33:50 +0200

    Added Tower skeleton class

---
+++ immunity/immunity/immunity/model/Tower.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace immunity
+{
+    class Tower
+    {
+        protected int cost;
+        protected int damage;
+        protected int level;
+        protected int range;
+
+        public int Cost
+        {
+            get { return cost; }
+        }
+
+        public int Damage
+        {
+            get { return damage; }
+        }
+
+        public int Level
+        {
+            get { return level; }
+        }
+
+        public int Range
+        {
+            get { return range; }
+        }
+
+        public Tower()
+        {
+
+        }
+
+        public void draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
