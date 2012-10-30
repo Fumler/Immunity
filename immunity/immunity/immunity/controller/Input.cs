d1a4c4fbdcff69e12a7ba0b77af9042d1ccdddd1
Fredrik Pettersen <fredrik.fumler@gmail.com>
2012-10-30 20:11:31 +0100

    Changed button.cs, input.cs Added getters for mouse in input and tried getting mouseclick to work in button but I'm retarded

--- immunity/immunity/immunity/controller/Input.cs
+++ immunity/immunity/immunity/controller/Input.cs
@@ -3,17 +3,101 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
 
 namespace immunity
 {
     class Input
     {
+        /// <summary>
+        /// Handles the states of the mouse and keyboard.
+        /// </summary>
         KeyboardState previousKeyState, currentKeyState;
         MouseState previousMouseState, currentMouseState;
 
-        public bool IsMouseOnePressed() {
-            return (currentMouseState.LeftButton == ButtonState.Pressed);
+
+        ////////////////////////////////////////////////////////////////////////// MOUSE
+
+        /// <summary>
+        /// The coordinates of the mouse
+        /// </summary>
+        public Vector2 Position
+        {
+            get;
+            protected set;
         }
+
+        /// <summary>
+        /// Checks if the left button is pressed
+        /// </summary>
+        public bool leftClick
+        {
+            get { return currentMouseState.LeftButton == ButtonState.Pressed; }
+        }
+
+        /// <summary>
+        /// Checks if the left button is clicked now but not before
+        /// </summary>
+        public bool newLeftClick
+        {
+            get
+            {
+                return currentMouseState.LeftButton == ButtonState.Pressed
+                  && previousMouseState.LeftButton == ButtonState.Released;
+            }
+        }
+
+        /// <summary>
+        /// Checks if the left button was released
+        /// </summary>
+        public bool releaseLeftClick
+        {
+            get { return !leftClick && previousMouseState.LeftButton == ButtonState.Pressed; }
+        }
+
+        /// <summary>
+        /// Checks if the right button is pressed
+        /// </summary>
+        public bool rightClick
+        {
+            get { return currentMouseState.RightButton == ButtonState.Pressed; }
+        }
+
+        /// <summary>
+        /// Checks if the right button is clicked now but not before
+        /// </summary>
+        public bool newRightClick
+        {
+            get
+            {
+                return currentMouseState.RightButton == ButtonState.Pressed
+                  && previousMouseState.RightButton == ButtonState.Released;
+            }
+        }
+
+        /// <summary>
+        /// Checks if the right button was released
+        /// </summary>
+        public bool releaseRightClick
+        {
+            get {return !rightClick && previousMouseState.RightButton == ButtonState.Pressed;}
+        }
+
+        public bool normalState
+        {
+            get { return !leftClick && !releaseLeftClick && !rightClick && !releaseRightClick; }
+        }
+
+        ////////////////////////////////////////////////////////////////////////// KEYBOARD
+
+        public bool isKeyPressed(Keys key)
+        {
+            return (currentKeyState.IsKeyDown(key));
+        }
+
+
+        ////////////////////////////////////////////////////////////////////////// OTHER
+
 
         public void Update() {
             previousKeyState = currentKeyState;
@@ -21,6 +105,8 @@ namespace immunity
 
             previousMouseState = currentMouseState;
             currentMouseState = Mouse.GetState();
+
+            Position = new Vector2(currentMouseState.X, currentMouseState.Y);
         }
     }
 }
--- immunity/immunity/immunity/model/Button.cs
+++ immunity/immunity/immunity/model/Button.cs
@@ -5,23 +5,75 @@ using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Input;
 
 
 namespace immunity
 {
 
+    public enum MouseStatus
+    {
+        Normal,
+        Clicked,
+        Released
+    }
+
     class Button
     {
-        public bool pressed = false;
+        Input mouse = new Input();
+
+        private MouseState previousState;
 
         private List<Texture2D> buttons;
 
         private Rectangle bounds;
 
+        private MouseStatus state = MouseStatus.Normal;
+
         public Button(Rectangle bounds) 
         {
             this.bounds = bounds;
         }
+
+        public void Update(GameTime gameTime)
+        {
+
+            // do it here instead of mouse class to test
+            MouseState mouseState = Mouse.GetState();
+
+            int mouseX = mouseState.X;
+            int mouseY = mouseState.Y;
+
+            bool isMouseOver = bounds.Contains(mouseX, mouseY);
+
+            if (isMouseOver && !mouse.leftClick)
+                state = MouseStatus.Released;
+            else if (!isMouseOver && !mouse.leftClick)
+                state = MouseStatus.Normal;
+
+            if (mouse.newLeftClick)
+            {
+                if (isMouseOver)
+                {
+                    state = MouseStatus.Clicked;
+                }
+            }
+
+            if (mouse.releaseLeftClick)
+            {
+                if (isMouseOver)
+                {
+                    state = MouseStatus.Released;
+                }
+            }
+            else if (state == MouseStatus.Clicked)
+            {
+                state = MouseStatus.Normal;
+            }
+            
+        }
+
+        
 
         public void setSprites(List<Texture2D> sprites)
         {
@@ -30,7 +82,23 @@ namespace immunity
 
         public void draw(SpriteBatch spriteBatch, int texture)
         {
-            spriteBatch.Draw(buttons[texture], bounds, Color.White);
+            switch (state)
+            {
+                case MouseStatus.Normal:
+                    spriteBatch.Draw(buttons[texture], bounds, Color.White);
+                    
+                    break;
+                case MouseStatus.Released:
+                    spriteBatch.Draw(buttons[texture], bounds, Color.Blue);
+                    break;
+                case MouseStatus.Clicked:
+                     spriteBatch.Draw(buttons[texture], bounds, Color.Red);
+                    break;
+                default:
+                    spriteBatch.Draw(buttons[texture], bounds, Color.White);
+              
+                    break;
+            }
         }
     }
 }
