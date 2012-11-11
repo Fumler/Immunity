d21f2126553fbdd8eef14601e134b31e2aed1dc1
Fredrik Pettersen <fredrik.fumler@gmail.com>
2012-11-11 16:07:54 +0100

    Minor fixes

--- immunity/immunity/immunity/model/ScreenObjects.cs
+++ immunity/immunity/immunity/model/ScreenObjects.cs
@@ -1,5 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
 
 namespace immunity
 {
